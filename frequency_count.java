dc0650f77237ad372bb1a9164bd7b74cad75b529
sharmasukanya14 <48435520+sharmasukanya14@users.noreply.github.com>
2019-06-26 00:43:10 +0530

    Add files via upload

---
+++ frequency_count.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+class TestClass 
+{
+   
+	 public static void main(String args[] ) throws Exception {
+  
+   	 Scanner sc = new Scanner(System.in);
+      
+     
+	 int n = sc.nextInt();
+     
+	ArrayList<Integer> a = new ArrayList<Integer>();
+   
+ 	 for(int i=0;i<n;i++)
+    
+	 {
+    
+    	 int s= sc.nextInt();
+      
+  	 a.add(s);
+  
+ 	  }
+        
+ 	 int m= sc.nextInt();
+   
+      	 ArrayList<Integer> b = new ArrayList<Integer>();
+
+    
+   	 for(int i=0;i<m;i++)
+       
+	 {
+       
+ 	 int t =sc.nextInt();
+        
+	 b.add(t);
+          
+ 	 	if(a.contains(t))
+          
+ 	 	{
+               
+ 		System.out.println(Collections.frequency(a,t));
+         
+  		 }
+          
+ 		 else
+  {
+   
+               		 System.out.println("NOT PRESENT");
+       
+  	  	 }
+
+      	  }
+ 
+       }
+}
